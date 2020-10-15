8ac7771ffd2e556aefd8104e3aa5acb3406e9b67
S A SAharukh <imsaharukh@Saharukhs-iMac-Pro.local>
2020-10-16 00:10:18 +0600

    heroku update

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 
 
